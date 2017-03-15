14a9770e2667cf9f508d2c763f220847d374f9d9
yaphone <zhouyaphone@163.com>
2017-03-16 00:49:04 +0800

    日常开发

--- helloweb Maven Webapp/src/test/java/cn/zhouyafeng/test/MyTest.java
+++ helloweb Maven Webapp/src/test/java/cn/zhouyafeng/test/MyTest.java
@@ -1,10 +1,12 @@
 package cn.zhouyafeng.test;
 
+import com.github.rjeschke.txtmark.Processor;
+
 public class MyTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String html = Processor.process("#Hello World");
+		System.out.println(html);
 	}
 
 }
