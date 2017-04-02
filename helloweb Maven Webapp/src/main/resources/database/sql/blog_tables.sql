2827276a736c715cf036cab7a37002b2f9254dee
yaphone <zhouyaphone@163.com>
2017-04-02 23:18:55 +0800

    添加数据模型

---
+++ helloweb Maven Webapp/src/main/resources/database/sql/blog_tables.sql
@@ -0,0 +1,57 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     2017/4/2 23:18:03                            */
+/*==============================================================*/
+
+
+drop table if exists blog;
+
+drop table if exists blog_comment;
+
+drop table if exists sub_comment;
+
+/*==============================================================*/
+/* Table: blog                                                  */
+/*==============================================================*/
+create table blog
+(
+   id                   bigint not null,
+   title                char(255),
+   content              text,
+   publish_time         date,
+   update_time          date,
+   classify             char(255),
+   keywords             char(255),
+   music_url            char(255),
+   reading_count        bigint,
+   primary key (id)
+);
+
+/*==============================================================*/
+/* Table: blog_comment                                          */
+/*==============================================================*/
+create table blog_comment
+(
+   id                   bigint not null,
+   nick_name            char(255),
+   publish_time         date,
+   content              text,
+   email                char(255),
+   blog_id              bigint,
+   primary key (id)
+);
+
+/*==============================================================*/
+/* Table: sub_comment                                           */
+/*==============================================================*/
+create table sub_comment
+(
+   id                   bigint not null,
+   comment_id           bigint not null,
+   nick_name            char(255),
+   content              text,
+   email                char(255),
+   publish_time         date,
+   primary key (id)
+);
+
